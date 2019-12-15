b11e4b87f72a2bf03aae82ae7d7c8c7d44fe59b3
kaznachieva1989 <kaznachieva1989@gmail.com>
2019-12-15 17:57:19 +0600

    Корректировка вывода в консоль

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -14,23 +14,25 @@ public class Runner extends Thread {
             System.out.printf("%s берет палочку \n", Thread.currentThread().getName() + " " + i);
             if (j == 6) {
                 System.out.printf("%s бежит к финишу \n", Thread.currentThread().getName() + " " + i);
+                System.out.printf("%s бежит к Runner 4 \n", Thread.currentThread().getName() + " " + i);
             } else {
                 System.out.printf("%s бежит к Runner %d \n", Thread.currentThread().getName() + " " + i, j);
                 try {
-                    Thread.sleep(5000);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     System.out.println("Thread has been interrupted");
                 }
             }
         }
-        for (int i = 5, j = 5; i >= 1; i--, j--) {
+        for (int i = 4, j = 4; i >= 1; i--, j--) {
             System.out.printf("%s берет палочку \n", Thread.currentThread().getName() + " " + i);
             if (i == 1) {
                 interrupt();
-            } else {
+            }
+            else {
                 System.out.printf("%s бежит к Runner %d \n", Thread.currentThread().getName() + " " + i, j);
                 try {
-                    Thread.sleep(5000);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     System.out.println("Thread has been interrupted");
                 }
