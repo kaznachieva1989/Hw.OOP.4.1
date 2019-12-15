7e88b09eeba4377086f522bca84d764d78e62390
kaznachieva1989 <kaznachieva1989@gmail.com>
2019-12-15 14:44:17 +0600

    Корректировка вывода в консоль

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         new Runner("Runner").start();
 
--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Runner extends Thread {
     public Runner(String name) {
         super(name);
@@ -7,18 +9,30 @@ public class Runner extends Thread {
 
     @Override
     public void run() {
-        while (true) {
-            for (int i = 1, j = 2; i < 6; i++, j++) {
-                System.out.printf("%s берет палочку \n", Thread.currentThread().getName() + " " + i);
-                if (j == 6) {
-                    System.out.printf("%s бежит к финишу \n", Thread.currentThread().getName() + " " + i);
-                } else {
-                    System.out.printf("%s бежит к Runner %s \n", Thread.currentThread().getName() + " " + i, j);
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        System.out.println("Thread has been interrupted");
-                    }
+
+        for (int i = 1, j = 2; i < 6; i++, j++) {
+            System.out.printf("%s берет палочку \n", Thread.currentThread().getName() + " " + i);
+            if (j == 6) {
+                System.out.printf("%s бежит к финишу \n", Thread.currentThread().getName() + " " + i);
+            } else {
+                System.out.printf("%s бежит к Runner %d \n", Thread.currentThread().getName() + " " + i, j);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println("Thread has been interrupted");
+                }
+            }
+        }
+        for (int i = 5, j = 5; i >= 1; i--, j--) {
+            System.out.printf("%s берет палочку \n", Thread.currentThread().getName() + " " + i);
+            if (i == 1) {
+                interrupt();
+            } else {
+                System.out.printf("%s бежит к Runner %d \n", Thread.currentThread().getName() + " " + i, j);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println("Thread has been interrupted");
                 }
             }
         }
